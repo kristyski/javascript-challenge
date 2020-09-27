3b60bf28990fd4d41226808be03246033415050b
Autzen <kristyskisfast@gmail.com>
2020-09-27 11:10:55 -0700

    latest

--- static/js/app.js
+++ static/js/app.js
@@ -2,36 +2,33 @@
 var tableData = data;
 
 // from activity 14/03/02
-// Use D3 to select the table
-let table = d3.select("ufo-table");
+// Use D3 to select the table; may not need this
+// let table = d3.select("ufo-table");
 
-// Use D3 to select the table body
-let tablebody = d3.select("tbody");
+// Use D3 to select the table body; try var if let doesn't work
+let tableBody = d3.select("tbody");
 
+tableData.forEach(function(sighting) {
+    console.log(sighting);
 
-// Loop through and call the printName function
-// for (var i = 0; i < tableData.length; i++) {
-//     printName(tableData[i]);
-//   }
-  
-// Append one table row `tr` to the table body
-    let row = tablebody.append("tr");
+    // Append one table row for each sighting
+    var row = tableBody.append("tr");
 
-// // Append one cell for the datetime
-    row.append("td").text(tableData[0];
+    // Console.log each sighting
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
 
-// // Append one cell for the city
-    row.append("td").text(tableData[1]);
+    });
 
-
-
-// // let table2 = d3.select(".grades");
-// console.log(tableData);
+  });
 
 // // Use d3 to create a bootstrap striped table
 // // http://getbootstrap.com/docs/3.3/css/#tables-striped
 // // table.attr("class", "table table-striped");
-
 
 // Prevent the page from refreshing
 // d3.event.preventDefault();
